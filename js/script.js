71fc2b013e53e5c3e3f4defb4660d2d5ca2e240d
Yuzu Saijo <rubberyuzu@Yuzus-MacBook-Pro.local>
2015-05-30 16:57:33 +0900

    initial commit- created about me page so far

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(function(){
+  // scroll programming
+  var $mynameCode = $("h1.my_name_typed");
+  var $myName = $mynameCode.text();
+  var cursor = "<span id='cursor' />";
+  var charWidth = 5;
+  $(window).scroll(function(){
+    $mynameCode.css('visibility', 'visible');
+    $mynameCode.html(cursor);
+    $scroll = $(this).scrollTop();
+    
+    if($scroll < 250){
+      $(".test").text($scroll);
+      $mynameCode.html($myName.substring(0, Math.round($scroll/15))+ cursor);
+    }else if($scroll < 300){
+      //complete name
+      $mynameCode.html($myName + cursor);
+    }else if($scroll < 550){
+      $mynameCode.html($myName.substring(0, Math.round((550-$scroll)/15))+ cursor);
+    }else if($scroll < 800){
+      $mynameCode.html(("ABOUT "+ $myName).substring(0, Math.round(($scroll-550)/15))+ cursor);
+    }else if($scroll < 3000){
+      //about + complete name
+      $mynameCode.html("ABOUT "+ $myName + cursor);
+    }
+  });
+
+
+
+
+
+
+
+
+})
