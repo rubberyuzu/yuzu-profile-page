acfef64bd9f162d95737a266e44939d551bed5eb
Yuzu Saijo <rubberyuzu@Yuzus-MacBook-Pro.local>
2015-06-02 15:28:45 +0900

    working on skills page. wind chime effect done implementing

--- js/script.js
+++ js/script.js
@@ -16,12 +16,17 @@ $(function(){
       //complete name
       $mynameCode.html($myName + cursor);
     }else if($scroll < 550){
+      //deleting
       $mynameCode.html($myName.substring(0, Math.round((550-$scroll)/15))+ cursor);
     }else if($scroll < 800){
       $mynameCode.html(("ABOUT "+ $myName).substring(0, Math.round(($scroll-550)/15))+ cursor);
     }else if($scroll < 3000){
       //about + complete name
       $mynameCode.html("ABOUT "+ $myName + cursor);
+    }else if($scroll < 3500){
+      $mynameCode.html(("ABOUT "+$myName).substring(0, Math.round((3500-$scroll)/15))+ cursor);
+    }else if($scroll < 5500){
+      $mynameCode.html(($myName + "'S SKILLS").substring(0, Math.round(($scroll-3500)/15))+ cursor);
     }
   });
 
