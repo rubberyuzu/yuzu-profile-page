5dd6925de9efc2445a04b77907b9eb9e92021dff
Yuzu Saijo <rubberyuzu@Yuzus-MacBook-Pro.local>
2015-06-18 13:02:09 +0900

    almost done

--- js/script.js
+++ js/script.js
@@ -54,15 +54,92 @@ $(function(){
     }else if($scroll < 14500){
       //contact + complete name
       $mynameCode.html("CONTACT "+ $myName + cursor);
+      $(".sns_logo").css("pointer-events", "auto");
     }
+
+    if($scroll < 13800){
+      $(".sns_logo").css("pointer-events", "none");
+    }else if ($scroll < 14000){
+      delete_noise();
+      init_called = false;
+    }else if(($scroll <14100) && ($scroll>14000) && (init_called == false)){
+      init();
+    }else if($scroll > 14100){
+      animate();
+    }
+
   });
 
 
-
-
-
-
-
+      var elems = [];
+      var init_called = false;
+      function init() {
+        if(init_called == false){
+        elems = [];
+        for(var i = 0; i < 60; i++) {
+          var startValue = (Math.random() - Math.random()) * 2 +25 ;
+          var endValue = 40+ (Math.random() - Math.random()) * 2;
+          var domElement = document.createElement('div');
+          domElement.setAttribute("class","random_noise");
+          var bg = (Math.random() * 0xffffff) >> 0;
+          domElement.style.position = 'fixed';
+          domElement.style.top = (Math.random() * window.innerHeight) + 'px';
+          domElement.style.left = startValue + '%';
+          // domElement.style.background= Colors.random().rgb;
+          domElement.style.background = '#' + bg.toString(16);
+          domElement.style.width = '40%';
+          domElement.style.height = '0.5px';
+          var elem = { x: startValue, domElement: domElement };
+          var updateCallback = function() {
+            this.domElement.style.left = this.x + '%';
+          }
+          var tween = new TWEEN.Tween(elem)
+            .to({ x: endValue }, 2000)
+            .delay(Math.random() * 1000)
+            .onUpdate(updateCallback)
+            .easing(TWEEN.Easing.Sinusoidal.InOut)
+            .start();
+          var tweenBack = new TWEEN.Tween(elem, false)
+            .to({ x: startValue}, 2000)
+            .delay(Math.random() * 1000)
+            .onUpdate(updateCallback)
+            .easing(TWEEN.Easing.Sinusoidal.InOut);
+          tween.chain(tweenBack);
+          tweenBack.chain(tween);
+          document.body.appendChild(elem.domElement);
+          elems.push(elem);
+        }
+        init_called = true;
+      }
+      }
+      function animate( time ) {
+        requestAnimationFrame( animate );
+        TWEEN.update( time );
+      }
+      function delete_noise(){
+        $(".random_noise").remove();
+        console.log("surely removed");
+      }
+    // Colors = {};
+    // Colors.names = {
+    //     aqua: "#00ffff",
+    //     azure: "#f0ffff",
+    //     blue: "#0000ff",
+    //     darkblue: "#00008b",
+    //     darkgreen: "#006400",
+    //     green: "#008000",
+    //     lightblue: "#add8e6",
+    //     lightgreen: "#90ee90",
+    //     lime: "#00ff00",
+    // };
+    // Colors.random = function() {
+    //     var result;
+    //     var count = 0;
+    //     for (var prop in this.names)
+    //         if (Math.random() < 1/++count)
+    //            result = prop;
+    //     return { name: result, rgb: this.names[result]};
+    // };
 
 
 
