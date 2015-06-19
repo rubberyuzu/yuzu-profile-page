c625c8cab2075e5b96f3331a852928a8d6b435aa
Yuzu Saijo <rubberyuzu@Yuzus-MacBook-Pro.local>
2015-06-19 16:22:00 +0900

    mobile link debuggin

--- js/script_for_smartphone.js
+++ js/script_for_smartphone.js
@@ -2,11 +2,11 @@ $(function(){
   $(window).scroll(function(){
     $scroll = $(this).scrollTop();
     
-    if($scroll < 11400){
-      $(".sns_logo").css("pointer-events", "none");
-    }else if($scroll < 14500){
-      $(".sns_logo").css("pointer-events", "auto");
-    }    
+    // if($scroll < 11400){
+    //   $(".sns_logo").css("pointer-events", "none");
+    // }else if($scroll < 14500){
+    //   $(".sns_logo").css("pointer-events", "auto");
+    // }    
   });
 
 })
